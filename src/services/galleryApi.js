e60ef1a62d82b39fd430fdf528cf979e25f6093b
pereztjacob <pereztjacob@gmail.com>
2018-03-06 10:21:32 -0800

    api file connects to server, create css files for styles

---
+++ src/services/galleryApi.js
@@ -0,0 +1,23 @@
+// const user = 'Jacob';
+// const URL = `https:// image-gallery-server.herokuapp.com/api/${user}`;
+
+// function load() {
+//   return fetch(URL)
+//     .then(response => response.json());
+// }
+
+// function add(image) {
+//   return fetch(URL, {
+//     method: 'POST',
+//     body: JSON.stringify(image),
+//     headers: {
+//       'content-type': 'application/json'
+//     },
+//   }).then(response => response.json());
+// }
+
+// function remove(id) {
+//   return fetch(`${URL}/${id}`. {
+//     method: 'DELETE',
+//   }).then(response => response.json);
+// }
